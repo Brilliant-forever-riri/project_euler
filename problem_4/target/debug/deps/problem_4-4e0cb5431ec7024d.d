d0141db71a7ae2595c05c6e02d8c989e0d9d8879
Brilliant-forever-riri <mugasiahilda@gmail.com>
2024-08-16 12:09:21 +0300

    update docs

---
+++ problem_4/target/debug/deps/problem_4-4e0cb5431ec7024d.d
@@ -0,0 +1,5 @@
+/home/codingriri/project_euler/problem_4/target/debug/deps/libproblem_4-4e0cb5431ec7024d.rmeta: src/main.rs
+
+/home/codingriri/project_euler/problem_4/target/debug/deps/problem_4-4e0cb5431ec7024d.d: src/main.rs
+
+src/main.rs:
---
+++ problem_4/target/debug/deps/problem_4-5c3a731e4a604f9d.d
@@ -0,0 +1,5 @@
+/home/codingriri/project_euler/problem_4/target/debug/deps/libproblem_4-5c3a731e4a604f9d.rmeta: src/main.rs
+
+/home/codingriri/project_euler/problem_4/target/debug/deps/problem_4-5c3a731e4a604f9d.d: src/main.rs
+
+src/main.rs:

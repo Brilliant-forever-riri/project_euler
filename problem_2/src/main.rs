91ce0ba54a5225f6a36fd0eb276ecd437cb56bf3
Brilliant-forever-riri <mugasiahilda@gmail.com>
2024-08-16 11:09:48 +0300

    solution 2 project euler

--- problem_2/src/main.rs
+++ problem_2/src/main.rs
@@ -1,5 +1,17 @@
 fn main() {
-    let mut vec= vec[]
-    
-   for h: u32 in 1..4000000;
+    let mut sum = 0;
+    let mut a = 1;
+    let mut b = 2;
+
+    while b <= 4000000 {
+        if b % 2 == 0 {
+            sum += b;
+        }
+
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+
+    println!("The sum of even-valued terms is: {}", sum);
 }

2219a9a8989770f4c9372abc85c3e40d4549e1fe
Brilliant-forever-riri <mugasiahilda@gmail.com>
2024-08-22 15:55:48 +0300

    solution to problem 3

--- problem_3/target/debug/deps/problem_3-2bd5bcfb902383bf.d
+++
@@ -1,5 +0,0 @@
-/home/codingriri/project_euler/problem_3/target/debug/deps/problem_3-2bd5bcfb902383bf: src/main.rs
-
-/home/codingriri/project_euler/problem_3/target/debug/deps/problem_3-2bd5bcfb902383bf.d: src/main.rs
-
-src/main.rs:
--- problem_3/target/debug/problem_3.d
+++
@@ -1,1 +0,0 @@
-/home/codingriri/project_euler/problem_3/target/debug/problem_3: /home/codingriri/project_euler/problem_3/src/main.rs
